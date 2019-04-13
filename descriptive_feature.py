c20dc3341cb43ab684548e7c5129a34336d44a6b
4lrz <alirezarezaie0101@gmail.com>
2019-04-13 17:51:59 +0430

    initial

---
+++ descriptive_feature.py
@@ -0,0 +1,35 @@
+import statistics as st
+
+
+def min(number_list):
+    return min(number_list)
+
+
+def max(number_list):
+    return max(number_list)
+
+
+def range_domain(number_list):
+    return max(number_list) - min(number_list)
+
+
+def mean(number_list):
+    return st.mean(number_list)
+
+
+def mode(number_list):
+    return st.mode(number_list)
+
+
+def median(number_list):
+    return st.mmedian(number_list)
+
+
+def variance(number_list):
+    return st.variance(number_list)
+
+
+def deviation(number_list):
+    return st.stdev(number_list)
+
+
