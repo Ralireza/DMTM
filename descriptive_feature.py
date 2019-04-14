81bb9a948bfe0a6b3ca57e0da70cf24b91890f09
4lrz <alirezarezaie0101@gmail.com>
2019-04-14 12:53:03 +0430

    descriptive is done

--- descriptive_feature.py
+++ descriptive_feature.py
@@ -1,5 +1,6 @@
 import statistics as st
 from scipy import stats as ss
+import numpy as np
 
 
 def min(number_list):
@@ -37,3 +38,21 @@ def deviation(number_list):
 def trimmed_mean(number_list, limitation):
     # limitation is float number like 0.3 and delete 0.3 of outlier data
     return ss.trim_mean(number_list, limitation)
+
+
+def quantile(number_list, q):
+    # q between 0 , 1
+    return np.quantile(number_list, q)
+
+
+def population_skewness(number_list):
+    # choolegi
+    return ss.skew(number_list)
+
+
+def kurtosis(number_list):
+    # keshidegi
+    return ss.kurtosis(number_list)
+
+
+
