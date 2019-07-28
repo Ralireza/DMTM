4e8ce85746f662e46b575c9ca632906f3fc9bee1
ralireza <alirezarezaie0101@gmail.com>
2019-07-28 18:02:34 +0430

    add mean flask

--- flask/descriptive_feature.py
+++ flask/descriptive_feature.py
@@ -2,6 +2,7 @@ import statistics as st
 from scipy import stats as ss
 import numpy as np
 
+
 def min_num(number_list):
     return min(number_list)
 
@@ -16,6 +17,11 @@ def range_domain(number_list):
 
 def mean(number_list):
     return st.mean(number_list)
+
+
+def trimmed_mean(number_list, limitation):
+    # limitation is float number like 0.3 and delete 0.3 of outlier data
+    return ss.trim_mean(number_list, limitation)
 
 
 def mode(number_list):
@@ -36,11 +42,6 @@ def variance(number_list):
 
 def deviation(number_list):
     return st.stdev(number_list)
-
-
-def trimmed_mean(number_list, limitation):
-    # limitation is float number like 0.3 and delete 0.3 of outlier data
-    return ss.trim_mean(number_list, limitation)
 
 
 def quantile(number_list, q):
