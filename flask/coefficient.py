d333abc1082b5a7997d6309a760ceea1f8eeab15
ralireza <alirezarezaie0101@gmail.com>
2019-07-29 12:33:14 +0430

    change coefficient directory

--- flask/coefficient.py
+++ flask/coefficient.py
@@ -1,9 +1,4 @@
-import os
 import statistics as st
-import time
-
-import pandas
-from flask import request, json, jsonify, abort
 from scipy import stats as ss
 import numpy as np
 import pandas as pd
