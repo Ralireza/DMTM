79a80b6ebc9fda15c1cd4fd11b2cae0d53a19577
ralireza <alirezarezaie0101@gmail.com>
2019-07-29 12:45:08 +0430

    add pearson & spearman & kendal to flask

--- flask/server.py
+++ flask/server.py
@@ -7,17 +7,9 @@ import os
 from scipy import stats as ss
 import frequency as freq
 import descriptive_feature as df
+import coefficient as co
 
 current_path = os.path.dirname(os.path.abspath(__file__))
-
-
-def pearson_correlation(list_number1, list_number2):
-    return ss.pearsonr(list_number1, list_number2)
-
-
-def trimmed_mean(number_list, limitation):
-    # limitation is float number like 0.3 and delete 0.3 of outlier data
-    return ss.trim_mean(number_list, limitation)
 
 
 def bad_request():
@@ -78,7 +70,6 @@ def frequency():
 
 
 # </editor-fold>
-
 
 # <editor-fold desc="descriptive_feature">
 
@@ -229,7 +220,7 @@ def tmean():
         params = None
         try:
             params = req_data['parameters']
-            mean1 = trimmed_mean(num_list1, params['limit'])
+            mean1 = df.trimmed_mean(num_list1, params['limit'])
             result = {"tmean": mean1}
         except Exception:
             # result = {"error": "bad param or no param"}
@@ -537,7 +528,9 @@ def kurtosis():
 
 # </editor-fold>
 
-@app.route("/api/v1/coefficient/pearson", methods=['GET', 'POST'])
+# <editor-fold desc="coefficients">
+
+@app.route("/api/v1/coefficient/pearson", methods=['POST'])
 def pearson():
     if request.method == 'POST':
         try:
@@ -556,7 +549,7 @@ def pearson():
                 if math.isinf(num_list2[i]):
                     num_list2[i] = 0
 
-            correlation, p_value = pearson_correlation(num_list1, num_list2)
+            correlation, p_value = co.pearson_correlation(num_list1, num_list2)
             result = {"correlation": correlation, "p_value": p_value}
         except Exception:
             # result = {"error": "bad param or no param"}
@@ -574,6 +567,87 @@ def pearson():
         }
         resp = jsonify(data)
         return resp
+
+
+@app.route("/api/v1/coefficient/spearman", methods=['POST'])
+def spearman_correlation():
+    if request.method == 'POST':
+        try:
+            req_data = request.get_json()
+            data_url = req_data['data_file']
+            csv = pandas.read_csv(data_url)
+            headers = csv.columns.values
+            num_list1 = list(csv[headers[0]])
+            num_list2 = list(csv[headers[1]])
+
+            # delete outlier by impute zero
+            for i in range(len(num_list1)):
+                if math.isinf(num_list1[i]):
+                    num_list1[i] = 0
+            for i in range(len(num_list2)):
+                if math.isinf(num_list2[i]):
+                    num_list2[i] = 0
+
+            correlation, p_value = co.spearman_correlation(num_list1, num_list2)
+            result = {"correlation": correlation, "p_value": p_value}
+        except Exception:
+            # result = {"error": "bad param or no param"}
+            bad_request()
+        directory = 'dmtm_responses'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        current_milli_time = lambda: int(round(time.time() * 1000))
+        res_path = directory + '/' + str(current_milli_time()) + '.json'
+        with open(res_path, 'w') as outfile:
+            json.dump(result, outfile)
+        data = {
+            'result_file': res_path,
+            'results': result
+        }
+        resp = jsonify(data)
+        return resp
+
+
+@app.route("/api/v1/coefficient/kendall", methods=['POST'])
+def kendalltau_correlation():
+    if request.method == 'POST':
+        try:
+            req_data = request.get_json()
+            data_url = req_data['data_file']
+            csv = pandas.read_csv(data_url)
+            headers = csv.columns.values
+            num_list1 = list(csv[headers[0]])
+            num_list2 = list(csv[headers[1]])
+
+            # delete outlier by impute zero
+            for i in range(len(num_list1)):
+                if math.isinf(num_list1[i]):
+                    num_list1[i] = 0
+            for i in range(len(num_list2)):
+                if math.isinf(num_list2[i]):
+                    num_list2[i] = 0
+
+            correlation, p_value = co.kendalltau_correlation(num_list1, num_list2)
+            result = {"correlation": correlation, "p_value": p_value}
+        except Exception:
+            # result = {"error": "bad param or no param"}
+            bad_request()
+        directory = 'dmtm_responses'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        current_milli_time = lambda: int(round(time.time() * 1000))
+        res_path = directory + '/' + str(current_milli_time()) + '.json'
+        with open(res_path, 'w') as outfile:
+            json.dump(result, outfile)
+        data = {
+            'result_file': res_path,
+            'results': result
+        }
+        resp = jsonify(data)
+        return resp
+
+
+# </editor-fold>
 
 
 if __name__ == '__main__':
